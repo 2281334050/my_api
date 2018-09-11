75710ab0e2f637b46e8f0b6177ea95b82877f325
王田 <kingwang@wangtiandeMacBook-Pro.local>
2018-09-11 17:36:41 +0800

    no message

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -38,7 +38,7 @@ class Api extends CI_Controller {
 					"msg"=>"账号或密码错误"
 				];
 		}
-		echo json_encode($token);
+		echo json_encode($query);
 	}
 	public function create_token($uname,$pwd){
 			$token = md5($uname,16).'-'.md5($uname,16).'-'.md5(time()+86400,16);
